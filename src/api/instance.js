d92e5609fcb4c59410fed8035d2f88d84b8c2c36
Ruslan Stupak <stupakruslan07@gmail.com>
2026-06-30 23:43:03 +0300

    fix issues from MR

--- src/api/instance.js
+++ src/api/instance.js
@@ -1,9 +1,7 @@
 import axios from 'axios';
 
-const BASE_URL = import.meta.env.VITE_API_BASE_URL ?? '';
-
 export const instance = axios.create({
-  baseURL: BASE_URL,
+  baseURL: 'https://your-energy.b.goit.study/api',
   headers: {
     Accept: 'application/json',
   },

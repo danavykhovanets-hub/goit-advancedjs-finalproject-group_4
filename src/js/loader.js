00ff89673a97dc306d828c9f62ea76b589af701e
danavykhovanets-hub <dana.vykhovanets@gmail.com>
2026-07-01 14:34:40 -0400

    Loader

---
+++ src/js/loader.js
@@ -0,0 +1,9 @@
+const loader = document.getElementById('loader');
+
+export function showLoader() {
+  loader.classList.add('loader--visible');
+}
+
+export function hideLoader() {
+  loader.classList.remove('loader--visible');
+}

b02881769964cc08e445357c58954caf321a6fce
Cocokringle <cocokringle.1@gmail.com>
2026-07-01 16:49:16 +0300

    Add StorageService for managing favorites in localStorage

---
+++ src/lib/storage.js
@@ -0,0 +1,54 @@
+export const FAVORITES_STORAGE_KEY = 'favorites';
+
+export class StorageService {
+  constructor(key, storage = localStorage) {
+    this.key = key;
+    this.storage = storage;
+  }
+
+  load = defaultValue => {
+    try {
+      const savedValue = this.storage.getItem(this.key);
+
+      if (savedValue === null) {
+        return defaultValue;
+      }
+
+      return JSON.parse(savedValue);
+    } catch (error) {
+      console.error(
+        `Failed to read data from localStorage by key "${this.key}"`,
+        error
+      );
+      return defaultValue;
+    }
+  };
+
+  save = value => {
+    try {
+      this.storage.setItem(this.key, JSON.stringify(value));
+      return true;
+    } catch (error) {
+      console.error(
+        `Failed to save data to localStorage by key "${this.key}"`,
+        error
+      );
+      return false;
+    }
+  };
+
+  remove = () => {
+    try {
+      this.storage.removeItem(this.key);
+      return true;
+    } catch (error) {
+      console.error(
+        `Failed to remove data from localStorage by key "${this.key}"`,
+        error
+      );
+      return false;
+    }
+  };
+}
+
+export const favoritesStorage = new StorageService(FAVORITES_STORAGE_KEY);
